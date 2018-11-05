e8dc89b0580600ad1016b644415300fbd271b67f
griffsking <griffking0325@hotmail.com>
2018-11-05 13:26:06 -0500

    adding reference folder

--- src/mainGame/Player.java
+++ src/mainGame/Player.java
@@ -143,6 +143,14 @@ public class Player extends GameObject {
 	public void setDamage(int damage) {
 		this.damage = damage;
 	}
+	
+	public double getX() {
+		return (int) this.x;
+	}
+	
+	public double getY() {
+		return (int) this.y;
+	}
 
 	public void setPlayerSize(int size) {
 		this.playerWidth = size;
